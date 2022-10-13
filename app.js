e3065cd03772b4573ade2cee6c93b62512435686
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-10-13 00:16:00 +0000

    required mongoose, created fruitsDB and added collections (fruits, people)

---
+++ app.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose");
+
+//creating a new database called fruitsDB
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
+});
+
+//declare schema and create model or collection (Fruits)
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit",
+});
+
+//fruit.save();
+
+////declare schema and create model or collection (People)
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "Alex",
+  age: 15,
+});
+
+person.save();
+
+//new fruits to be added to Database
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 10,
+  review: "The best fruit!",
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  score: 5,
+  review: "Too sour",
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  score: 7,
+  review: "I like bananas",
+});
+
+// Adding or Inserting more than fruit into Database
+// Fruit.insertMany([kiwi, orange, banana], function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved all the fruits to fruitsDB");
+//   }
+// });
+
+//Finding or Reading items(fruits) from Database
+Fruit.find(function (err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    fruits.forEach(function (fruit) {
+      console.log(fruit.name);
+
+      mongoose.connection.close();
+    });
+  }
+});
